4db07b3317680b22a103029a7035b5d65c9fec23
Jisun Park <jisunpark.t@gmail.com>
2024-12-31 15:50:19 +0900

    feat: 히스토리 이동 기능 추가

--- src/Game.jsx
+++ src/Game.jsx
@@ -2,12 +2,26 @@ import Board from "./Board";
 import { useGameStore } from "./store";
 
 export default function Game() {
-  const { history, xIsNext, setHistory, setXIsNext } = useGameStore();
-  const currentSquares = history[history.length - 1];
+  const {
+    history,
+    currentMove,
+    xIsNext,
+    setHistory,
+    setCurrentMove,
+    setXIsNext,
+  } = useGameStore();
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory(history.concat([nextSquares]));
+    const nextHistory = history.slice(0, currentMove + 1).concat([nextSquares]);
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(currentMove % 2 === 0);
   }
 
   return (
@@ -22,7 +36,22 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div style={{ marginLeft: "1rem" }}>
-        <ol>{/*TODO*/}</ol>
+        <ol>
+          {history.map((_, historyIndex) => {
+            const description =
+              historyIndex > 0
+                ? `Go to move #${historyIndex}`
+                : "Go to game start";
+
+            return (
+              <li key={historyIndex}>
+                <button onClick={() => jumpTo(historyIndex)}>
+                  {description}
+                </button>
+              </li>
+            );
+          })}
+        </ol>
       </div>
     </div>
   );
