6cb928dcac3d67970ddf8926aec52ba579e7d6ea
Jisun Park <jisunpark.t@gmail.com>
2024-12-31 11:23:41 +0900

    feat: 기본 게임 기능 완성

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,13 +1,17 @@
+import { calculateStatus, calculateTurns, calculateWinner } from "./logic";
 import { Square } from "./Square";
 import { useGameStore } from "./store";
 
 export default function Board() {
   const { squares, xIsNext, setSquares, setXIsNext } = useGameStore();
 
+  const winner = calculateWinner(squares);
+  const turns = calculateTurns(squares);
   const player = xIsNext ? "X" : "O";
+  const status = calculateStatus(winner, turns, player);
 
   function handleClick(i) {
-    if (squares[i]) return;
+    if (squares[i] || winner) return;
     const nextSquares = squares.slice();
     nextSquares[i] = player;
     setSquares(nextSquares);
@@ -15,23 +19,26 @@ export default function Board() {
   }
 
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 1fr)",
-        gridTemplateRows: "repeat(3, 1fr)",
-        width: "calc(3 * 2.5rem)",
-        height: "calc(3 * 2.5rem)",
-        border: "1px solid #999",
-      }}
-    >
-      {squares.map((square, squareIndex) => (
-        <Square
-          key={squareIndex}
-          value={square}
-          onSquareClick={() => handleClick(squareIndex)}
-        />
-      ))}
-    </div>
+    <>
+      <div style={{ marginBottom: "0.5rem" }}>{status}</div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          gridTemplateRows: "repeat(3, 1fr)",
+          width: "calc(3 * 2.5rem)",
+          height: "calc(3 * 2.5rem)",
+          border: "1px solid #999",
+        }}
+      >
+        {squares.map((square, squareIndex) => (
+          <Square
+            key={squareIndex}
+            value={square}
+            onSquareClick={() => handleClick(squareIndex)}
+          />
+        ))}
+      </div>
+    </>
   );
 }
